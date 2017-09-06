5f625b17af88d809ba2b65d114255b750ff51fc9
Matthew Hutchinson <matt@hiddenloop.com>
2017-09-06 20:23:27 +0100

    multi option searching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
   end
-  
+
   private
   def search_params
-    params.permit([:name, :instrument_id, :genre_id])
+    params.permit(:name, :genre_id => [], :instrument_id => [])
   end
 end
