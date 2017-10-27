f55d8cf545d1b16d6d942a383ed82aafcc147c16
mdunnettstone <mdunnettstone@gmail.com>
2017-10-27 09:55:46 +0000

    Minor tweaks mostly with big if statements for if 0 records

--- Gemfile
+++ Gemfile
@@ -71,7 +71,7 @@ gem 'carrierwave', '~> 1.0'
 
 gem 'rails-assets-sweetalert2', '~> 5.1.1', source: 'https://rails-assets.org'
 
-# gem 'sweet-alert2-rails'
+gem 'sweet-alert2-rails'
 
 gem 'font-awesome-rails'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require sweetalert2
+//= require sweet-alert2-rails
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
