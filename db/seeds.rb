5cbc7f1e111d2bd94d99bb7b6cdc31f147b0fadb
mdunnettstone <mdunnettstone@gmail.com>
2017-10-27 08:05:19 +0000

    Added basic db:seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#INSTRUMENTS
 %w(harp piano guitar drums violin trumpet kazoo).each do |instrument_name|
   Instrument.create(name: instrument_name)
 end
 
+#GENRES
 %w(jazz rock classical country hiphop).each do |genre_name|
   Genre.create(name: genre_name)
+end
+
+#FACILITIES
+# Instrument, brand, model
+facility_list = [
+["Piano", "Yamaha", "C3"],
+["Piano", "Beckstein", "CFX Concert"],
+["Amp", "Roland", "Cube Street"],
+["Drums", "Pearl", "EXX 22"],
+["Drums", "Pearl", "ePro EXX Fusion"]
+]
+
+facility_list.each do |instrument, brand, model|
+  Facility.create(instrument: instrument, brand: brand, model: model)
+end
+
+#ROOMS
+#name, address, capacty, facility_ids
+room_list = [
+["Room 1", "Seed Music UK Headquarters, London", 5, [1, 3, 5]],
+["Room 2", "Seed Music France headquarters, Paris", 25, [2, 4, 5]],
+["Room 3", "Royal Albert Hall, Kensington Gore, Kensington, London, SW7 2AP", 2000, [1, 2, 3, 4, 5]]
+]
+
+room_list.each do |name, address, capacity, facility_ids|
+  Room.create(name: name, address: address, capacity: capacity, facility_ids: facility_ids)
 end
