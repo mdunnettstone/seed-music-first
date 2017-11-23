43e37bbbee0c2be8687b1315eb7b037737f571c2
mdunnettstone <mdunnettstone@gmail.com>
2017-11-23 14:05:35 +0000

    Attempted error stuff, to little avail. A few front end tweaks

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -83,4 +83,16 @@ class Booking < ApplicationRecord
       BookingMailer.booking_reminder(self, user).deliver
     end
   end
+
+  def read_attribute_for_validation(attr)
+    send(attr)
+  end
+
+  def self.human_attribute_name(attr, options = {})
+    attr
+  end
+
+  def self.lookup_ancestors
+    [self]
+  end
 end
